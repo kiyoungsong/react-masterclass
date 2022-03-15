4aa115fb9f5a6963c8e096f3c04e247578930601
ky.song <ky.song@aerix.co.kr>
2022-03-15 15:02:37 +0900

    ⚡ feature: chart api 적용

--- src/api.ts
+++ src/api.ts
@@ -13,3 +13,9 @@ export async function fetchCoinInfo(coinId: string) {
 export async function fetchCoinTickers(coinId: string) {
   return await (await fetch(`${BASE_URL}/tickers/${coinId}`)).json();
 }
+
+export async function fetchCoinHistory(coinId: string) {
+  const endDate = Math.floor(Date.now() / 1000);
+  const startDate = endDate - 60 * 60 * 24 * 7 * 2; // 이주일전
+  return await(await fetch(`${BASE_URL}/coins/${coinId}/ohlcv/historical?start=${startDate}&end=${endDate}`)).json();
+}
--- src/routes/Charts.tsx
+++ src/routes/Charts.tsx
@@ -1,4 +1,11 @@
+import { useQuery } from "react-query";
+import { useParams } from "react-router-dom";
+import { fetchCoinHistory } from "../api";
+
 function Chart() {
+  const { coinId } = useParams();
+  const { isLoading, data } = useQuery(["ohlcv", coinId], () => fetchCoinHistory(coinId!));
+  console.log(data);
   return <h1>Chart</h1>;
 }
 
