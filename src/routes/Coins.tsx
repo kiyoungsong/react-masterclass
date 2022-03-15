660f554f64b839915b65fffad3ce1cb1a7ef9fd3
ky.song <ky.song@aerix.co.kr>
2022-03-15 17:01:55 +0900

    ⚡ feature: recoil 입문

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -20,10 +20,11 @@ const Header = styled.header`
 const CoinsList = styled.ul``;
 
 const Coin = styled.li`
-  background-color: white;
-  color: ${props => props.theme.bgColor};
+  background-color: ${(props) => props.theme.cardBgColor};
+  color: ${(props) => props.theme.textColor};
   border-radius: 15px;
   margin-bottom: 10px;
+  border: 1px solid white;
   a {
     display: flex;
     align-items: center;
@@ -32,7 +33,7 @@ const Coin = styled.li`
   }
   &:hover {
     a {
-      color: ${props => props.theme.accentColor};
+      color: ${(props) => props.theme.accentColor};
     }
   }
 `;
@@ -84,6 +85,7 @@ function Coins() {
       </Helmet>
       <Header>
         <Title>코인</Title>
+        <button>Toggle Mode</button>
       </Header>
       {isLoading ? (
         <Loader>Loading...</Loader>
