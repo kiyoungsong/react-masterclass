f8e1c90daa3da74191513c4a1c63752407c00086
ky.song <ky.song@aerix.co.kr>
2022-03-17 10:18:49 +0900

    ⚡ feature: validation error 적용

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -7,6 +7,8 @@ interface IForm {
   lastName: string;
   username: string;
   password: string;
+  password1: string;
+  extraError?: string;
 }
 
 function ToDoList() {
@@ -14,11 +16,13 @@ function ToDoList() {
   // watch은 변경을 주시함
   // handleSubmit => defaultPrevent, Validation담당
   // formState 에러를 표시함
+  // setError 특정 에러를 발생시킴
   const {
     register,
     watch,
     handleSubmit,
     formState: { errors },
+    setError
   } = useForm <IForm>({
     defaultValues: {
       email: '@naver.com' // 이렇게 기본값을 지정해 줄수도 있다.
@@ -26,7 +30,11 @@ function ToDoList() {
   });
 
   const onValid = (data: any) => {
-    console.log(data);
+    if (data.password !== data.password1) {
+      return setError("password1", { message: "Password are not the same"}, { shouldFocus: true})
+    }
+    // 특정한 요소가 아닌 form전체 요소에 대한 에러임
+    setError("extraError", { message: "Server Offline"})
   }
   
   return (
@@ -39,7 +47,7 @@ function ToDoList() {
           {...register("email", {
             required: "Email is required",
             pattern: {
-              value: /^[A-Za-z0-9._%+-]+@naver.com$/,
+              value: /^[A-Za-z0-9._%+-]+@naver.com$/, // 정규식적용
               message: "Only naver.com emails allowed",
             },
           })}
@@ -48,6 +56,7 @@ function ToDoList() {
         <span>{errors?.email?.message}</span>
         <input
           {...register("firstName", { required: "firstName is required" })}
+          // 혹은 reuired: true 이런식으로 전달 가능
           placeholder="First Name"
         />
         <span>{errors?.firstName?.message}</span>
@@ -59,6 +68,11 @@ function ToDoList() {
         <input
           {...register("username", {
             required: "username is required",
+            // 객체를 이용해 여러개도 가능
+            validate: {
+              noNico: (value) => !value.includes("nico") || "no nico allowed!",
+              noNick: (value) => !value.includes("nick") || "no nick allowed!",
+            },
             minLength: {
               value: 3,
               message: "username length should be over 3",
@@ -78,7 +92,19 @@ function ToDoList() {
           placeholder="Password"
         />
         <span>{errors?.password?.message}</span>
+        <input
+          {...register("password1", {
+            required: "password is required",
+            minLength: {
+              value: 5,
+              message: "Password length should be over 5",
+            },
+          })}
+          placeholder="Password1"
+        />
+        <span>{errors?.password1?.message}</span>
         <button>Add</button>
+        <span>{errors?.extraError?.message}</span>
       </form>
     </div>
   );
