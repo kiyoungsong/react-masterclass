5525a126f8f7bb63a2b9dbe3102e12dc668b4379
ky.song <ky.song@aerix.co.kr>
2022-03-17 11:30:11 +0900

    ⚡ feature: 특정 배열 인덱스의 상태 값 변경

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,12 +1,38 @@
-import { IToDo } from "./atoms";
+import React from "react";
+import { useSetRecoilState } from "recoil";
+import { IToDo, toDoState } from "./atoms";
 
-function ToDo({text}: IToDo) {
+
+function ToDo({text, category, id}: IToDo) {
+  const setToDos = useSetRecoilState(toDoState);
+  // 타입스크립트 팁 newCategory: IToDo["category"]
+  const onClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const { name } = e.currentTarget;
+    setToDos((oldToDos) => {
+      const targetIndex = oldToDos.findIndex(toDo => toDo.id === id);
+      const newTodo = { text, id, category: name as IToDo["category"] };
+      
+      return [...oldToDos.slice(0, targetIndex), newTodo, ...oldToDos.slice(targetIndex + 1)];
+    })
+  };
   return (
     <li>
       <span>{text}</span>
-      <button>Doing</button>
-      <button>To Do</button>
-      <button>Done</button>
+      {category !== "DOING" && (
+        <button name="DOING" onClick={onClick}>
+          Doing
+        </button>
+      )}
+      {category !== "TO_DO" && (
+        <button name="TO_DO" onClick={onClick}>
+          To Do
+        </button>
+      )}
+      {category !== "DONE" && (
+        <button name="DONE" onClick={onClick}>
+          Done
+        </button>
+      )}
     </li>
   );
 }
