a99bab5e87608dd1b6a02bed209abc3bc2546092
ky.song <ky.song@aerix.co.kr>
2022-03-17 10:31:35 +0900

    ⚡ feature: toDo list react-hook-form 적용

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -2,6 +2,29 @@ import React, { useState } from "react";
 import { useForm } from 'react-hook-form';
 
 interface IForm {
+  toDo: string;
+}
+
+function ToDoList() {
+  const { register, handleSubmit, setValue } = useForm<IForm>();
+  const onSubmit = (data: IForm)  => {
+    console.log('add to do', data.toDo)
+    setValue("toDo", '')
+  }
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input {...register("toDo", { required: "Please write a to do" }) } placeholder="Write a to do" />
+        <button>Add</button>
+      </form>
+    </div>
+  );
+}
+
+/*
+ * react-hook-form 적용 
+ */
+interface IHookForm {
   email: string;
   firstName: string;
   lastName: string;
@@ -11,7 +34,7 @@ interface IForm {
   extraError?: string;
 }
 
-function ToDoList() {
+export function ToDoListReactHookForm() {
   // register가 있으면 onChange항수가 필요없음
   // watch은 변경을 주시함
   // handleSubmit => defaultPrevent, Validation담당
@@ -22,11 +45,11 @@ function ToDoList() {
     watch,
     handleSubmit,
     formState: { errors },
-    setError
-  } = useForm <IForm>({
+    setError,
+  } = useForm<IHookForm>({
     defaultValues: {
-      email: '@naver.com' // 이렇게 기본값을 지정해 줄수도 있다.
-    }
+      email: "@naver.com", // 이렇게 기본값을 지정해 줄수도 있다.
+    },
   });
 
   const onValid = (data: any) => {
@@ -108,28 +131,6 @@ function ToDoList() {
       </form>
     </div>
   );
-
-  // react Form을 통해 밑의 주석 코드는 필요없어짐
-  // const [toDo, setToDo] = useState('');
-
-  // const onChange = (e: React.FormEvent<HTMLInputElement>) => {
-  //   const { currentTarget: { value } } = e;
-  //   setToDo(value);
-  // };
-
-  // const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-  //   e.preventDefault();
-  //   console.log(toDo);
-  // }
-
-  // return (
-  //   <div>
-  //     <form onSubmit={onSubmit}>
-  //       <input value={toDo} onChange={onChange} placeholder="Write a to do" />
-  //       <button>Add</button>
-  //     </form>
-  //   </div>
-  // );
 }
 
 export default ToDoList;
