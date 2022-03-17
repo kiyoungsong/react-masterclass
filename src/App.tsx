ccf2c5464bb122ad6c121a98e1cc6e1c80c0858c
ky.song <ky.song@aerix.co.kr>
2022-03-17 14:34:18 +0900

    ⚡ feature: recoil set 연습

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,36 @@
 import React from 'react';
+import { useRecoilState } from 'recoil';
+import { hourSelector, minuteState } from './components/atoms';
 
 function App() {
+  const [minutes, setMinutes] = useRecoilState(minuteState);
+  const [hours, setHours] = useRecoilState(hourSelector);
+  const onMinutesChange = (e: React.FormEvent<HTMLInputElement>) => {
+    // 앞에 +를 붙여주면 숫자로 바뀜
+    setMinutes(+e.currentTarget.value)
+  }
+
+  const onHoursChange = (e: React.FormEvent<HTMLInputElement>) => {
+    // 앞에 +를 붙여주면 숫자로 바뀜
+    setHours(+e.currentTarget.value);
+  };
   return (
-    <>
-    </>
+    <div>
+      <form>
+        <input
+          value={minutes}
+          onChange={onMinutesChange}
+          type="number"
+          placeholder="Minutes"
+        />
+        <input
+          value={hours}
+          onChange={onHoursChange}
+          type="number"
+          placeholder="Hours"
+        />
+      </form>
+    </div>
   );
 }
 
