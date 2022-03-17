dd5415303a362e2af8dbc4b80e524de4e05ef537
ky.song <ky.song@aerix.co.kr>
2022-03-17 10:45:50 +0900

    ⚡ feature: toDoList recoil 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createGlobalStyle } from 'styled-components';
-import ToDoList from './TodoList';
+import ToDoList from './components/TodoList';
 
 const GlobalStyle = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,22 +1,42 @@
 import React, { useState } from "react";
 import { useForm } from 'react-hook-form';
+import { atom, useRecoilState, useRecoilValue } from "recoil";
 
 interface IForm {
   toDo: string;
 }
 
+interface IToDo {
+  id: number;
+  text: string;
+  category: "TO_DO" | "DOING" | "DONE";
+}
+
+const toDoState = atom<IToDo[]>({
+  key: "toDo",
+  default: [],
+})
+
+
 function ToDoList() {
+  const [toDos, setToDos] = useRecoilState(toDoState);
   const { register, handleSubmit, setValue } = useForm<IForm>();
-  const onSubmit = (data: IForm)  => {
-    console.log('add to do', data.toDo)
-    setValue("toDo", '')
+  const onSubmit = ({ toDo }: IForm)  => {
+    setToDos(oldToDos => [{text: toDo, id: Date.now(), category: "TO_DO" },...oldToDos])
+    setValue("toDo", '');
   }
+
+  console.log(toDos)
   return (
     <div>
+      <h1>To Dos</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
         <input {...register("toDo", { required: "Please write a to do" }) } placeholder="Write a to do" />
         <button>Add</button>
       </form>
+      <ul>
+        {toDos.map(toDo => <li key={toDo.id}>{toDo.text}</li>)}
+      </ul>
     </div>
   );
 }
